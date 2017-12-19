2d72044eb390e7006af2681eda72d53b48eacdc7
Josh Nijenhuis <josh@nijenhuis.ca>
2017-12-19 06:50:39 -0500

    Indicate land that bidder has highest bids on during hover

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var mapoffset = 150;
 var mapoffsetrequest = '-'+mapoffset+',-'+mapoffset+'/'+mapoffset+','+mapoffset;
 var width = (mapoffset*blocksize) * 2 + blocksize;
 var height = (mapoffset*blocksize) * 2 + blocksize;
-var landdata;
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 var svg = d3.select("#divbody").append("svg")
@@ -24,55 +23,41 @@ var div = d3.select("body").append("div")
 	.attr("class", "tooltip")
 	.style("opacity", 0);
 
+function highlightAddress (id, highlightclass) {
+		let addresses = svg.selectAll('#id-'+id);
+		addresses.attr("class", ""+ highlightclass);
+}
+
 axios.get('https://api.auction.decentraland.org/api/parcelState/range/'+mapoffsetrequest)
 	.then((res) => {
-		landdata = res.data.data;
-		console.log(landdata);
+		let landdata = res.data.data;
+
 		zoomLayer.selectAll("rect")
-		.data(landdata)
-		.enter()
-		.append("rect")
-		.attr("x", function(d){
-			return (d.x*blocksize)+(width/2)+1;
-		})
-		.attr("y", function(d){
-			return ((d.y*-1)*blocksize)+(height/2)+1;
-		})
-		.attr("height", blocksize)
-		.attr("width", blocksize)
-		.attr("fill", function(d) {return d.projectId !== null ? '#BFBFBF' : color(d.amount/100) ;})
-		.on("click", function(d) { console.log(d);})
-		.on("mouseover", function(d) {
-			div.transition().duration(100).style("opacity", 0.8);
-			div.html(d.id + "<br/>" + (d.projectId !== null ? 'Non biddable' : 'Amount:'+d.amount)).style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY - 27) + "px");
-		})
-		.on("mouseout", function(d) {
-			div.transition()
-			.duration(400)
-			.style("opacity", 0);
-		})
-		;
+			.data(landdata)
+			.enter()
+			.append("rect")
+			.attr("id", function (d) { return 'id-'+(d.address === null ? d.id : d.address);})
+			.attr("x", function(d){
+				return (d.x*blocksize)+(width/2)+1;
+			})
+			.attr("y", function(d){
+				return ((d.y*-1)*blocksize)+(height/2)+1;
+			})
+			.attr("height", blocksize)
+			.attr("width", blocksize)
+			.attr("fill", function(d) {return d.projectId !== null ? '#BFBFBF' : color(d.amount/100) ;})
+			.on("click", function(d) { console.log(d);})
+			.on("mouseover", function(d) {
+				div.transition().duration(100).style("opacity", 0.8);
+				div.html(d.id + "<br/>" + (d.projectId !== null ? 'Non biddable' : 'Amount:'+d.amount)).style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY - 27) + "px");
+				highlightAddress(d.address,'highlightAddress');
+			})
+			.on("mouseout", function(d) {
+				div.transition()
+				.duration(400)
+				.style("opacity", 0);
+				highlightAddress(d.address,'');
+			})
+			;
 	}
 );
-
-d3.select("#generate")
-   .on("click", writeDownloadLink);
-
-function writeDownloadLink(){
-	var html = d3.select("svg")
-		.attr("title", "decentraland-map")
-		.attr("version", 1.1)
-		.attr("xmlns", "http://www.w3.org/2000/svg")
-		.node().parentNode.innerHTML;
-
-	var blob = new Blob([html], {type: "image/svg+xml"});
-   var url = window.URL.createObjectURL(blob);
-
-	var a = document.createElement("a");
-	document.body.appendChild(a);
-	a.style = "display: none";
-	a.href = url;
-	a.download = "decentraland-map.svg";
-	a.click();
-	window.URL.revokeObjectURL(url);
-}
